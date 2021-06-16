5f39a0acada4ad0e41aedc16ae356ad144569f30
Michael <yhm2046@gmail.com>
2021-06-16 10:35:17 +0800

    Initial commit

---
+++ app/src/main/java/com/cnd/zhongkong/qiyeban/RightFragment.kt
@@ -0,0 +1,20 @@
+package com.cnd.zhongkong.qiyeban
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * 右侧布局
+ */
+class RightFragment:Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.right_fragment,container,false)
+    }
+}
