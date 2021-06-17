ec8afad161bad8635a48e5af880539b33a9003cc
Michael <yhm2046@gmail.com>
2021-06-17 17:33:43 +0800

    优化检查网络是否正常的登录界面

--- app/src/main/java/com/cnd/zhongkong/qiyeban/ActivityLogin.kt
+++ app/src/main/java/com/cnd/zhongkong/qiyeban/ActivityLogin.kt
@@ -22,9 +22,7 @@ class ActivityLogin : AppCompatActivity() {
     private var intentFilter: IntentFilter? = null
     private var networkChangeReceiver: ActivityLogin.NetworkChangeReceiver? = null
     private val BROAD_NETWORK = "android.net.conn.CONNECTIVITY_CHANGE"
-    private val btnLogin: Button? = null
-    private val tvLoginState: TextView? = null
-    val TAG  = "ActivityLogin"
+    val TAG  = "ActivityLogin:xwg"
     public val NETWORK_OK = 1
     public val NETWORK_ERROR = 2
     public val LOGIN_OK = 3
@@ -32,19 +30,13 @@ class ActivityLogin : AppCompatActivity() {
     val handler: Handler = object : Handler() {
         override fun handleMessage(msg: Message) {
             when (msg.what) {
-                NETWORK_OK -> text!!.text = "网络正常"
+                NETWORK_OK -> text!!.text = "网络正常，可以扫码登录"
                 NETWORK_ERROR -> text!!.text = "网络异常，请检查"
-                LOGIN_OK -> startActivity(
-                    Intent(
-                        this@ActivityLogin,
-                        MainActivity::class.java
-                    )
-                )
-                LOGIN_ERROR -> Toast.makeText(
-                    applicationContext,
-                    "网络异常，请确认网络正常后再扫码登录",
-                    Toast.LENGTH_LONG
-                ).show()
+                LOGIN_OK -> {
+                    Log.i(TAG,"enter code windows")
+//                    startActivity(Intent(this@ActivityLogin,MainActivity::class.java))
+                }
+                LOGIN_ERROR -> Toast.makeText(applicationContext,"网络异常，请确认网络正常后再扫码登录",Toast.LENGTH_LONG).show()
             }
         }
     }
@@ -53,15 +45,20 @@ class ActivityLogin : AppCompatActivity() {
         val binding=ActivityLoginBinding.inflate(layoutInflater);
         setContentView(binding.root)
         text=findViewById(R.id.tvLoginState)
+        binding.btnLogin!!.setOnClickListener {
+            Log.i(TAG,"click btn...")
+            val msg = Message()
+            if (flagNetworkState) msg.what = LOGIN_OK
+            else msg.what =LOGIN_ERROR
+            handler.sendMessage(msg)
+        }
 //        注册系统广播
-        //        注册系统广播
         intentFilter = IntentFilter()
         intentFilter!!.addAction(BROAD_NETWORK)
         networkChangeReceiver = NetworkChangeReceiver()
         registerReceiver(networkChangeReceiver, intentFilter)
     }
 
-  //广播类
   //    广播接收者
   inner class NetworkChangeReceiver : BroadcastReceiver() {
       override fun onReceive(context: Context, intent: Intent) {
@@ -69,12 +66,12 @@ class ActivityLogin : AppCompatActivity() {
           val connectivityManager = context.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
           val networkInfo = connectivityManager.activeNetworkInfo
           if (networkInfo != null && networkInfo.isAvailable) {
-              Log.i("xwg", "send msg network is ok")
-              message.what = 1
+              Log.i(TAG, "send msg network is ok")
+              message.what = NETWORK_OK
               flagNetworkState = true
           } else {
-              Log.i("xwg", "send msg network is error")
-              message.what = 2
+              Log.i(TAG, "send msg network is error")
+              message.what = NETWORK_ERROR
               flagNetworkState = false
           }
           this@ActivityLogin.handler.sendMessage(message)
