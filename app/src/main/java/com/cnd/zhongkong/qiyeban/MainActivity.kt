06a0be95602a336c765fedf37c43ada02d929ad0
Michael <yhm2046@gmail.com>
2021-06-17 14:33:27 +0800

    去掉多余的文件，还是新闻应用

--- app/src/main/java/com/cnd/zhongkong/qiyeban/MainActivity.kt
+++ app/src/main/java/com/cnd/zhongkong/qiyeban/MainActivity.kt
@@ -7,11 +7,13 @@ import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.cnd.zhongkong.qiyeban.databinding.ActivityMainBinding
 
+/**
+ * 主界面，载入分屏
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding=ActivityMainBinding.inflate(layoutInflater);
         setContentView(binding.root)
-//        setContentView(R.layout.activity_main)
         }
 }
